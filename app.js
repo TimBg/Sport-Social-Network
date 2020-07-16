448972c1db719db998e3e08a84587b146073d758
TimBg <best.green.zebra@yandex.ua>
2020-07-16 15:47:12 +0300

    'edit'

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+app.use('/api/auth', require('./routes/auth.routes'));
+
 const PORT = config.get('port') || 5000;
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUri'));
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, () => console.log(`Server is working on port ${PORT}`));
     } catch(e) {
         console.log('Server Error', e.message);
         process.exit(1);
     }
 }
 
-app.listen(PORT, () => console.log(`Server is working on port ${PORT}`));
+start();
